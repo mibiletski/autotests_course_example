3c1594723b83bf9f4c9007bdf16b2b124d8e857e
TENSOR-CORP\mi.bileckiy <23442@mail.ru>
2023-05-09 18:44:07 +0300

    новые изменения

--- Homework3/task2.py
+++ Homework3/task2.py
@@ -4,8 +4,11 @@
 
 из  статистики  импортировать  среднее
 защита  get_list_info ( lst ):
-    # Здесь пишем код
-    вернуть  min_elem , max_elem , sum_list , среднее значение
+    min_elem = min(lst)
+    max_elem = max(lst)
+    sum_list = sum(lst)
+    average = round(sum(lst)/len(lst), 2)
+    return min_elem, max_elem, sum_list, average
 
 # НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 
